6fd905316b021948d0d83c75889d7e3f97a0ed33
lmsweli32-boop <lmsweli32@ai.wethinkcode.co.za>
2026-03-23 03:33:15 +0200

    Add merge function for sorted array merging
    Implement merge function to combine two sorted arrays.

---
+++ taskAISolution.js
@@ -0,0 +1,29 @@
+Final verified solution
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  while (i < left.length) {
+    result.push(left[i]);
+    i++; // Fixed
+  }
+
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+
+  return result;
+}
